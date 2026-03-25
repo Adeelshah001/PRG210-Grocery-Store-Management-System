99dd53995edcbaf2d91a4af9cdb649f30afd1af0
Jasmeet Singh Saluja <50644805+jssaluja1@users.noreply.github.com>
2026-03-25 12:42:48 -0400

    Implement user interface selection in main function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 
 int main() {
+    Interface* ui = nullptr;
+    int choices;
+    do {
+        cout << "\n" <<"\n1. Customer\n2. Manager\n 3.Exit\n";
+        cin >> choices;
+        if(choices==1){
+            ui = new CustomerInterface();
+            ui->showMenu();
+        }
+        else if(choices == 2){
+            ui = new ManagerInterface();
+            ui->showMenu();
+		}else
+        {
+            break;
+        }
+        delete ui;
+
+    } while (choices != 3);
+	delete[] StoreDeoartments;
     cout << "PRG210 Group Project Setup Successful!" << endl;
-    return 0;
+    return 0;;
 }
