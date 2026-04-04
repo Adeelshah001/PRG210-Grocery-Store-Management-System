3a22afaea54b78c953b619db02a60291e25b147c
Jasmeet Singh Saluja <50644805+jssaluja1@users.noreply.github.com>
2026-04-04 06:39:06 -0400

    Update interface.cpp

---
+++ interface.cpp
@@ -0,0 +1,21 @@
+#include "Interface.h"
+#include <iostream>
+using namespace std;
+
+int Interface::getValidChoice(int min, int max)
+{
+    int choice ;
+    while( true)
+    {
+        cin>>choice;
+        if(choice >= min && choice <= max)// basic range check 
+        {
+            return choice;
+        }
+        else
+        {
+            cout<<"Invalid input, Please enter again:";
+        }
+
+    }
+}
