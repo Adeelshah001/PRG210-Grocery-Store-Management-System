ada429bb916e339d0b9b36c8b04b8fec44ccd253
Jasmeet Singh Saluja <50644805+jssaluja1@users.noreply.github.com>
2026-04-04 06:38:23 -0400

    Add ManagerInterface class with basic methods

---
+++ managerinterface.h
@@ -0,0 +1,18 @@
+#include "Interface.h"
+#include "department.h"
+#include "fileHandling.h"
+#include "product.h"
+#include <iostream>
+using namespace std;
+
+class ManagerInterface : public Interface
+{
+private:
+    void listDepartments();
+    void addDepartment();
+    void addItem();
+    void saveChanges();
+
+public:
+    void showMenu();  // override base class menu
+};
